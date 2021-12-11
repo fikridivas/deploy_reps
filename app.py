3da1676757d48602f1222f9ee72d63838ccf261f
Fikri Diva <45113535+fikridivas@users.noreply.github.com>
2021-12-11 16:00:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -51,6 +51,6 @@ def predict():
     # elif prediction[0]=="2":
     #     output=="Jelek"
 
-    return render_template('deploy.html',pm10=pm10,pm25=pm25,so2=so2,co=co,o3=o3,no2=no2,air_quality=soutput)
+    return render_template('deploy.html',pm10=pm10,pm25=pm25,so2=so2,co=co,o3=o3,no2=no2,air_quality=output)
 if __name__=='__main__':
     app.run(debug=False)
